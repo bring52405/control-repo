53a8626e9a3a33bd5e45c07e9c8a01547659d5ac
Brian Ring <brian-ring@uiowa.edu>
2020-04-29 11:39:32 -0500

    e

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,4 +2,12 @@ class profile::r10k {
   class {'r10k':
     remote => 'https://github.com/bring52405/control-repo',
   }
+  class {'r10K::webhook::config':
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class {'r10k::webhook':
+    user  => 'root',
+    group => 'root',
+  }
 }
