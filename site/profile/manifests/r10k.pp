afad4e469d462a51b34435a95a189067b13a4c52
bring52405 <39968370+bring52405@users.noreply.github.com>
2020-04-29 11:24:24 -0500

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,12 +2,4 @@ class profile::r10k {
   class {'r10k':
     remote => 'https://github.com/bring52405/control-repo',
   }
-  class {'r10K::webhook::config':
-    use_mcollective => false,
-    enable_ssl      => false,
-  }
-  class {'r10k::webhook':
-    user  => 'root',
-    group => 'root',
-  }
 }
